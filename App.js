8e5d823f10a9ace05161fcdf7665d58973f3ba76
VineBast <vine.bast@gmail.com>
2022-06-15 18:32:29 +0200

    first commit - bare project with google API

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +26,13 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  config.resolve.alias['react-native'] = 'react-native-web';
+  config.resolve.alias['react-native-maps'] = 'react-native-web-maps';
+  // Customize the config before returning it.
+  return config;
+};
