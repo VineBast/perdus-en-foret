4a075947caf7edb255f15c3515f53690bb11968e
VineBast <vine.bast@gmail.com>
2022-06-21 12:17:48 +0200

    refresh PRS on map when moving

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,6 +11,7 @@ import * as Location from 'expo-location';
 
 export function HomeScreen({ navigation }) {
   const [location, setLocation] = useState(null);
+  const [regionChanged, setRegionChanged] = useState();
   const [errorMsg, setErrorMsg] = useState(null);
   const user = useSelector(userSelector).user;
   const dataPRS = useState(data.features); //dataPRS.geometry.coordinates[1] = latitude , dataPRS.geometry.coordinates[0] = longitude
@@ -19,28 +20,28 @@ export function HomeScreen({ navigation }) {
 
   //Coordonnées par points du Nord-Ouest jusqu'au Sud-Ouest dans le sens des aiguilles d'une montre
   //dans le cas présent les points font un rectangle en forme de smartphone autour de l'Ile-de-France
-/*   const [coordinates] = useState([
-    //Point 0 : Nord-Ouest
-    {
-      latitude: 49.334252,
-      longitude: 1.942471,
-    },
-    //Point 1 : Nord-Est
-    {
-      latitude: 49.334252,
-      longitude: 2.63375,
-    },
-    //Point 2 : Sud-Est
-    {
-      latitude: 48.639048,
-      longitude: 2.63375,
-    },
-    //Point 3 : Sud-Ouest
-    {
-      latitude: 48.639048,
-      longitude: 1.942471,
-    },
-  ]); */
+  /*   const [coordinates] = useState([
+      //Point 0 : Nord-Ouest
+      {
+        latitude: 49.334252,
+        longitude: 1.942471,
+      },
+      //Point 1 : Nord-Est
+      {
+        latitude: 49.334252,
+        longitude: 2.63375,
+      },
+      //Point 2 : Sud-Est
+      {
+        latitude: 48.639048,
+        longitude: 2.63375,
+      },
+      //Point 3 : Sud-Ouest
+      {
+        latitude: 48.639048,
+        longitude: 1.942471,
+      },
+    ]); */
 
   useEffect(() => {
     (async () => {
@@ -49,13 +50,11 @@ export function HomeScreen({ navigation }) {
         setErrorMsg('Permission to access location was denied');
         return;
       }
-
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
       setFilteredDataPRS(filterDataPRS(location));
     })();
   }, []);
-
   let text = 'Waiting..';
   if (errorMsg) {
     text = errorMsg;
@@ -66,10 +65,10 @@ export function HomeScreen({ navigation }) {
 
   function filterDataPRS(location) {
     console.log(location);
-    let latitudeNord = location.coords.latitude + 0.5;
-    let latitudeSud = location.coords.latitude - 0.5;
-    let longitudeOuest = location.coords.longitude - 0.5;
-    let longitudeEst = location.coords.longitude + 0.5;
+    let latitudeNord = location.coords.latitude + 0.2;
+    let latitudeSud = location.coords.latitude - 0.2;
+    let longitudeOuest = location.coords.longitude - 0.2;
+    let longitudeEst = location.coords.longitude + 0.2;
     return dataPRS[0].filter(
       (elm) =>
         elm.geometry.coordinates[0] < longitudeEst &&
@@ -84,35 +83,35 @@ export function HomeScreen({ navigation }) {
       <OptionButton navigation={navigation} />
       <View style={style.container}>
         {
-        location && (
-          <MapView
-          focusable={true}
-          showsUserLocation={true}
-          followUserLocation={true}
-          style={style.map}
-          initialRegion={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.504757,
-            longitudeDelta: 0.506866,
-          }}
-        >
-          {filteredDataPRS.map((marker, i) => (
-            <MapView.Marker
-              key={i}
-              coordinate={{
-                latitude: marker.geometry.coordinates[1],
-                longitude: marker.geometry.coordinates[0],
+          location && (
+            <MapView
+              focusable={true}
+              showsUserLocation={true}
+              followUserLocation={true}
+              style={style.map}
+              onRegionChangeComplete={(status) => setFilteredDataPRS(filterDataPRS({ 'coords' : { latitude: status.latitude, longitude: status.longitude } }))}
+              initialRegion={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.504757,
+                longitudeDelta: 0.506866,
               }}
-              title={marker.properties.llib_prs}
-              description={marker.properties.libs_prs}
-              pinColor={colors.orange}
-            />
-          ))}
-        </MapView>
-         )
+            >              
+              {filteredDataPRS.map((marker, i) => (
+                <MapView.Marker
+                  key={i}
+                  coordinate={{
+                    latitude: marker.geometry.coordinates[1],
+                    longitude: marker.geometry.coordinates[0],
+                  }}
+                  title={marker.properties.llib_prs}
+                  description={marker.properties.libs_prs}
+                  pinColor={colors.orange}
+                />
+              ))}
+            </MapView>
+          )
         }
-
       </View>
       <ItineraryPlannedModal navigation={navigation} />
     </Background>
