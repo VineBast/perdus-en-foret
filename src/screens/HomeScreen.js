ddd5d595d944291a094c9ac9c16dfa3256c9f659
VineBast <vine.bast@gmail.com>
2022-06-21 12:36:32 +0200

    add function filterPRSFourPoints at services

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,34 +14,8 @@ export function HomeScreen({ navigation }) {
   const [regionChanged, setRegionChanged] = useState();
   const [errorMsg, setErrorMsg] = useState(null);
   const user = useSelector(userSelector).user;
-  const dataPRS = useState(data.features); //dataPRS.geometry.coordinates[1] = latitude , dataPRS.geometry.coordinates[0] = longitude
+  const dataPRS = data.features; //dataPRS.geometry.coordinates[1] = latitude , dataPRS.geometry.coordinates[0] = longitude
   const [filteredDataPRS, setFilteredDataPRS] = useState(data.features);
-
-
-  //Coordonnées par points du Nord-Ouest jusqu'au Sud-Ouest dans le sens des aiguilles d'une montre
-  //dans le cas présent les points font un rectangle en forme de smartphone autour de l'Ile-de-France
-  /*   const [coordinates] = useState([
-      //Point 0 : Nord-Ouest
-      {
-        latitude: 49.334252,
-        longitude: 1.942471,
-      },
-      //Point 1 : Nord-Est
-      {
-        latitude: 49.334252,
-        longitude: 2.63375,
-      },
-      //Point 2 : Sud-Est
-      {
-        latitude: 48.639048,
-        longitude: 2.63375,
-      },
-      //Point 3 : Sud-Ouest
-      {
-        latitude: 48.639048,
-        longitude: 1.942471,
-      },
-    ]); */
 
   useEffect(() => {
     (async () => {
@@ -69,7 +43,7 @@ export function HomeScreen({ navigation }) {
     let latitudeSud = location.coords.latitude - 0.2;
     let longitudeOuest = location.coords.longitude - 0.2;
     let longitudeEst = location.coords.longitude + 0.2;
-    return dataPRS[0].filter(
+    return dataPRS.filter(
       (elm) =>
         elm.geometry.coordinates[0] < longitudeEst &&
         elm.geometry.coordinates[0] > longitudeOuest &&
